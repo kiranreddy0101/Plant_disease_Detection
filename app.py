167173269a189386b4a7b330dbb226faa8526bad
kiranreddy0101 <148518139+kiranreddy0101@users.noreply.github.com>
2025-07-27 16:30:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,8 +192,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     img = image.resize((224, 224))
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
