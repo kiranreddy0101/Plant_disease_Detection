06780f07a4a78c7fd1b617a7a7385524d0cb2886
kiranreddy0101 <148518139+kiranreddy0101@users.noreply.github.com>
2025-08-01 21:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ import base64
 from io import BytesIO
 import cv2
 
-# Grad-CAM Functions
 def get_gradcam_heatmap(model, img_array, last_conv_layer_name, pred_index=None):
     grad_model = tf.keras.models.Model(
         [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
     )
     with tf.GradientTape() as tape:
         conv_outputs, predictions = grad_model(img_array)
+        predictions = tf.squeeze(predictions)  # [num_classes]
         if pred_index is None:
-            pred_index = tf.argmax(predictions[0])
-        class_channel = predictions[:, pred_index]
+            pred_index = tf.argmax(predictions)
+        class_channel = predictions[pred_index]
+
     grads = tape.gradient(class_channel, conv_outputs)
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
     conv_outputs = conv_outputs[0]
+
     heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
     heatmap = tf.squeeze(heatmap)
+
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy()
 
